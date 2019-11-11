ae74cb410c6965459d9e9175e78304409f8b71ab
Oscar Montes <oscar6932124>
2019-11-11 15:33:48 -0600

    Avance ruta estudiantes

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,21 @@
+const dbConfig = require('../config/db');
+const db = dbConfig.db;
 module.exports = (app, passport) => {
     app.get('/login', (req, res) => {
         console.log(req.flash('loginMessage')[0]);
         res.render('login', {loginMessage: req.flash('loginMessage')});
     });
     app.post('/login', passport.authenticate('local-login', {
-        successRedirect: '/dashboard',
+        successRedirect: '/asignaciones',
         failureRedirect: '/login',
         failureFlash: true
     }));
+    app.get('/asignaciones', (req, res) => {
+        res.render('asignaciones');
+    });
+    app.get('/estudiantes', async (req, res) => {
+        const allStudents = await db.collection('students').listDocuments();
+        res.render('estudiantes', allStudents.map(e => e.id));
+        console.log(allStudents.map(e => e.id));
+    });
 };
